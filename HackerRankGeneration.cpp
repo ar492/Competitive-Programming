6d955659dca25fe0b66eb9d338281dfa6655df1a
ar492 <47195443+ar492@users.noreply.github.com>
2020-07-20 23:11:20 -0400

    Update and rename potwTemplate.cpp to HackerRankGeneration.cpp

--- HackerRankGeneration.cpp
+++ HackerRankGeneration.cpp
@@ -8,7 +8,7 @@ using namespace std;
 string input[10]={"input01.txt","input02.txt","input03.txt","input04.txt","input05.txt","input06.txt","input07.txt","input08.txt","input09.txt","input10.txt"};
 string output[10]={"output01.txt","output02.txt","output03.txt","output04.txt","output05.txt","output06.txt","output07.txt","output08.txt","output09.txt","output10.txt"};
 
-    ///UNIFORM from a to b inclusive
+    ///UNIFORM random generation from a to b inclusive (10 times, for example)
     /*
     srand(time(NULL));
     for(int i=0; i<10; i++){
@@ -18,21 +18,24 @@ string output[10]={"output01.txt","output02.txt","output03.txt","output04.txt","
     */
 void genInput(){
     srand(time(NULL));
-    for(int i=0; i<10; i++){
+    for(int i=0; i<10; i++){ 
         ofstream fout(input[i]);
+        //Edit the following part to generate input.
+        /*
         int r = 1 + (rand() % static_cast<int>(10 - 1 + 1));
         fout<<r<<endl;
         for(int j=0; j<r; j++){
             int k=100 + (rand() % static_cast<int>(200 - 100 + 1));
             fout<<k<<" ";
         }
+        */
     }
 }
 void exePgrm(){
     for(int qq=0; qq<10; qq++){
         ifstream fin(input[qq]);
         ofstream fout(output[qq]);
-
+        //Put program here
     }
 
 
