71562c0e0f9cf4e574e8558a515fdf67ab3821be
ar492 <47195443+ar492@users.noreply.github.com>
2020-07-21 18:53:10 -0400

    Update HackerRank_IO_Generator.cpp

--- Coding Hub/HackerRank_IO_Generator.cpp
+++ Coding Hub/HackerRank_IO_Generator.cpp
@@ -1,45 +1,40 @@
 #include <bits/stdc++.h>
-#define pb push_back
-#define INF (int)1e9
-#define endl "\n"
-typedef long long ll;
 using namespace std;
 
 string input[10]={"input01.txt","input02.txt","input03.txt","input04.txt","input05.txt","input06.txt","input07.txt","input08.txt","input09.txt","input10.txt"};
 string output[10]={"output01.txt","output02.txt","output03.txt","output04.txt","output05.txt","output06.txt","output07.txt","output08.txt","output09.txt","output10.txt"};
 
-    ///UNIFORM random generation from a to b inclusive (10 times, for example)
-    /*
-    srand(time(NULL));
-    for(int i=0; i<10; i++){
-        int r = a + (rand() % static_cast<int>(b - a + 1));
-        cout<<r<<endl;
-    }
-    */
+int randAtoB(int a, int b){///Uniform distribution
+    return(((double) rand() / (RAND_MAX+1)) * (b-a+1) + a);
+}
+
 void genInput(){
-    srand(time(NULL));
-    for(int i=0; i<10; i++){ 
+    for(int i=0; i<10; i++){
         ofstream fout(input[i]);
-        //Edit the following part to generate input.
-        /*
-        int r = 1 + (rand() % static_cast<int>(10 - 1 + 1));
-        fout<<r<<endl;
-        for(int j=0; j<r; j++){
-            int k=100 + (rand() % static_cast<int>(200 - 100 + 1));
-            fout<<k<<" ";
-        }
-        */
+        int r=randAtoB(1, 10); fout<<r<<endl;
+        for(int j=0; j<r; j++)
+            if(j!=r-1)
+                fout<<randAtoB(1, 10)<<" ";
+            else
+                fout<<randAtoB(1, 10)<<endl;
     }
 }
 void exePgrm(){
     for(int qq=0; qq<10; qq++){
         ifstream fin(input[qq]);
         ofstream fout(output[qq]);
-        //Put program here
+        int n; fin>>n;
+        int accum=0;
+        for(int i=0; i<n; i++){
+            int k; fin>>k; accum+=k;
+        }
+        fout<<accum<<endl;
     }
 }
 int main(){
+    srand(time(NULL));
     genInput();
     exePgrm();
+
     return 0;
 }
