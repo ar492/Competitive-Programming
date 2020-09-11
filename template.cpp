835a2bed4a7e4577c63c45d85e8a119699eab243
ar492 <47195443+ar492@users.noreply.github.com>
2020-09-11 00:12:40 -0400

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    freopen("mootube.in", "r", stdin);
+    freopen("mootube.out", "w", stdout);
+
+return 0;
+}
