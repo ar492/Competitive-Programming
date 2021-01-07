7cf88d17cdf4bb6d34e877292fc7bbee3d846d79
Arpan Banerjee <47195443+ar492@users.noreply.github.com>
2021-01-06 23:39:20 -0500

    Update DSU.cpp

--- Templates/Graphs/DSU.cpp
+++ Templates/Graphs/DSU.cpp
@@ -14,18 +14,19 @@ void initialize(){
         parent[i] = i; size[i] = 1;
     }
 }
-int find_set(int v) {//finds the parent (representative) of a set
-    if (v == parent[v]) return v;
-    return parent[v] = find_set(parent[v]);
+
+int rep(int v) {
+     if (v == parent[v]) return v;
+     return parent[v] = rep(parent[v]);
 }
-void union_sets(int a, int b) {
-    a = find_set(a); b = find_set(b);
-    if (a != b) {
-        if (size[a] < size[b]) // merge by size
-            swap(a, b);
+
+void unite(int a, int b) {
+     a = rep(a); b = rep(b);
+     if (a != b) {
+        if (size[a] < size[b]) swap(a, b);
         parent[b] = a;
         size[a] += size[b];
-    }
+     }
 }
 
 int main(){
