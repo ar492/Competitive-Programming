27af4d7ad3ea843a268570f79feac6dea3e7fe9a
Arpan Banerjee <47195443+ar492@users.noreply.github.com>
2021-01-28 16:19:48 -0500

    Update linked_list.cpp

--- Templates/Miscellaneous/linked_list.cpp
+++ Templates/Miscellaneous/linked_list.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 
 struct node{
-        int val; node *to;
+        int val; node *to=NULL;
         void link(node &b){ to=&b; }
-        node next(){ return(*to); }
+        node next(){ return (*to); }
 };
 
 int main(){
-
+	node a; a.val=1;
+	if(a.to==NULL) cout<<"no next"<<endl;
+        node b;
+        b.val=4;
+        a.link(b);
+        cout<<a.next().val<<endl;
         return 0;
 }
