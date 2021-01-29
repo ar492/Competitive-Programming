b9b39ca43e21c6a0ae9dba759bc5f37334325f2a
Arpan Banerjee <47195443+ar492@users.noreply.github.com>
2021-01-28 21:12:18 -0500

    Create spaced.cpp

---
+++ USACO/Silver/2021/January/spaced.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int g[1000][1000], hans, vans, n;
+
+int rowSpace(int row, int sum=0){ for(int i=1; i<n; i+=2) sum+=g[row][i]; return sum; } //space to start the row
+int rowNoSpace(int row, int sum=0){ for(int i=0; i<n; i+=2) sum+=g[row][i]; return sum; } // C to start the row
+int colSpace(int col, int sum=0){ for(int i=0; i<n; i+=2) sum+=g[i][col]; return sum; } // space to start the column
+int colNoSpace(int col, int sum=0){ for(int i=1; i<n; i+=2) sum+=g[i][col]; return sum; } // C to start the column
+
+int main(){
+	      cin>>n;
+        for(int i=0; i<n; i++) for(int j=0; j<n; j++) cin>>g[i][j];
+        for(int i=0; i<n; i++){
+                hans+=max(rowSpace(i), rowNoSpace(i));
+                vans+=max(colSpace(i), colNoSpace(i));
+        }
+        cout<<max(hans, vans)<<endl;
+}
