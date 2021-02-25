e62d4c1581e954e0aa01da9f9df6c08778d53963
Arpan Banerjee <47195443+ar492@users.noreply.github.com>
2021-02-24 21:23:50 -0500

    Update Kruskal.cpp

--- Templates/Graphs/MST/Kruskal.cpp
+++ Templates/Graphs/MST/Kruskal.cpp
@@ -1,44 +1,24 @@
 // https://cses.fi/problemset/task/1675/
+
 #include<bits/stdc++.h>
 #define int long long
 #define pb push_back // replacing this with emplace_back doesn't work
 using namespace std;
  
-const int sz=1e6;
-int n,m;
+const int sz=1e5+1;
+int n,m, cmps;
  
 namespace DSU{
        int par[sz], size[sz];
-       void initialize(){
-              for(int i=0; i<n; i++) par[i]=i, size[i]=1;
-       }
+       void initialize(){ for(int i=0; i<n; i++) par[i]=i, size[i]=1; }
        int rep(int v){//finds the parent (representative) of a set
-              if (v == par[v]) return v;
-              return par[v] = rep(par[v]);
+              if (v==par[v]) return v;
+              return par[v]=rep(par[v]);
        }
        void unite(int a, int b){
-              a = rep(a); b = rep(b);
-              if(a != b){
-                     if (size[a] < size[b]) swap(a, b);
-                     par[b] = a; size[a] += size[b];
-              }
-       }
-}
- 
-namespace IsThereMST{
-       vector<int> adj[sz];
-       bool vis[sz];
-       void dfs(int node){
-              vis[node]=true;
-              for(int i:adj[node]) if(!vis[i]) dfs(i);
-       }
-       bool mst(){
-              int cmp=0;
-              for(int i=1; i<=n; i++){
-                     if(vis[i]) continue;
-                     dfs(i); cmp++;
-              }
-              return cmp==1;
+              a=rep(a); b=rep(b);
+              if (size[a] < size[b]) swap(a, b);
+              par[b]=a; size[a]+=size[b];
        }
 }
  
@@ -50,7 +30,6 @@ namespace kruskals{
        vector<edge> edges; int total;
 }
  
-using namespace IsThereMST;
 using namespace DSU;
 using namespace kruskals;
  
@@ -58,7 +37,6 @@ signed main(){
        cin>>n>>m;
        while(m--){
               int u,v,w; cin>>u>>v>>w;
-              adj[u].pb(v); adj[v].pb(u);
               edges.pb({u, v, w});
        }
        initialize();
@@ -67,11 +45,10 @@ signed main(){
        sort(edges.begin(), edges.end());
        for(edge e:edges){
               if(rep(e.u)==rep(e.v)) continue;
-              unite(e.u, e.v);
+              unite(e.u, e.v); cmps++;
               total+=e.w;
        }
- 
-       if(!mst()) puts("IMPOSSIBLE");
+       if(cmps!=n-1) puts("IMPOSSIBLE");
        else cout<<total<<endl;
 }
  
