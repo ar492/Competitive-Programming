c75ae84c3a1d8f61fcd21e0c936b1ce9c3bba6a5
Arpan Banerjee <47195443+ar492@users.noreply.github.com>
2021-03-03 11:51:09 -0500

    Create Terse.cpp
    short template for use with snippets/shortcuts

---
+++ Templates/Terse.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#define int long long
+#define what(x) cout <<"                                                  LINE "<<__LINE__<<"|>> "<<#x << " = " << (x)<<endl
+using namespace std;
+
+signed main(){
+       cin.tie(0)->sync_with_stdio(0);
+       
+}
