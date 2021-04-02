169d8d39dd5e732f0d31d1c4e412f0d68542a333
Arpan Banerjee <47195443+ar492@users.noreply.github.com>
2021-04-02 12:49:53 -0400

    Update Terse.cpp

--- Templates/Terse.cpp
+++ Templates/Terse.cpp
@@ -1,9 +1,12 @@
-#include<bits/stdc++.h>
-#define int long long
-#define what(x) cout <<"                                                  LINE "<<__LINE__<<"|>> "<<#x << " = " << (x)<<endl
+#ifdef LOCAL
+#include			<C:\\Users\\gbpol\\Desktop\\Useful\\cp.h>
+#else
+#include 			<bits/extc++.h> // stdc++
+#endif
+#define 			int long long
+#define 			_ cin.tie(0)->sync_with_stdio(0);cin.exceptions(ios_base::failbit);
 using namespace std;
 
-signed main(){
-       cin.tie(0)->sync_with_stdio(0);
-       
+signed main(){_
+	
 }
